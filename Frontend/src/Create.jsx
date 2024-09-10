07794de016f91b540ef2aa39cf1a192477b6c2ef
sreehhari <sreehari9300@gmail.com>
2024-09-10 22:40:42 +0530

    database connected

--- Frontend/src/Create.jsx
+++ Frontend/src/Create.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
+import axios from "axios"
 
 function Create() {
+    const[task,setTask]=useState();
+    const handleButton=()=>{
+        axios.post('http://localhost:3001/add',{task:task})
+        .then(result=>console.log(result))
+        .catch(err=>console.log(err))
+
+    }
   return (
     <div className="">
-        <input type="text" className="text rounded-full p-3 pl-6" />
+        <input type="text" className="text rounded-full p-3 pl-6"  onChange={(e)=>setTask(e.target.value)}/>
     <div className="flex flex-row p-4 justify-center ">
-        <button className="rounded-full bg-blue-600 text-white hover:bg-white hover:text-black font-bold py-2 px-4">Save</button>
+        <button onClick={handleButton} className="rounded-full bg-blue-600 text-white hover:bg-white hover:text-black font-bold py-2 px-4">Save</button>
         </div>
 
     </div> 
--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,24 @@
-const express = require("express");
-const {Todo} = require("./db")
-const { createTodo, markCompleted } = require("./types");
-const cors = require("cors")
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const todoModel=require('./Models/Todo')
+const dotenv=require('dotenv')
+
+dotenv.config();
 const app = express();
-
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-
-app.post("/todo",async function(req,res){
-    const createPayload = req.body;
-    const parsedPayload = createTodo.safeParse(createPayload);
-    if(!parsedPayload.success){
-        res.status(411).json({
-            msg:"wrong inputs vro"
-        })
-        return;
-    }
-    await Todo.create({
-        title:createPayload.title,
-        description:createPayload.description,
-        completed:false
-    });
-    res.json({
-        msg:'todo created'
-    })
-    
-});
-
-app.get("/todos",async function(req,res){
-     const todos = await Todo.find({});
-     res.json({
-        todos
-     });
-
-});
-
-app.put("/completed",async function(req,res){
-    const updatePayload=req.body;
-    const parsedPayload=markCompleted.safeParse(updatePayload);
-    if(!parsedPayload.success){
-        res.status(411).json({
-            msg:"not the right inputs vro...",
-        })
-        return;
-    }
-    await Todo.update({
-        _id:req.body.id
-    },
-    {
-        completed:true
-    });
-
-    res.json({
-        msg:'todo marked as completed'
-    })
-
-
-
+mongoose.connect(process.env.VITE_MONGO_URI)
+app.listen(3001,()=>{
+    console.log("running successfully")
 })
 
+app.post('/add',(req,res)=>{
+    const task = req.body.task;
+    todoModel.create({
+        task:task
 
-app.listen(3000);
+    }).then(result=>res.json(result))
+    .catch(err=>res.json(err))
+})
